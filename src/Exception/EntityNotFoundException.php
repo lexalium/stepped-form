c7ef8785770d4f0f4d93392dcb47822c3bac45b0
alex <allapkov@gmail.com>
2022-01-21 15:15:03 +0300

    Added getKey method to the EntityNotFoundException.

--- src/Exception/EntityNotFoundException.php
+++ src/Exception/EntityNotFoundException.php
@@ -8,8 +8,13 @@ use function sprintf;
 
 class EntityNotFoundException extends SteppedFormException
 {
-    public function __construct(string $key)
+    public function __construct(private string $key)
     {
-        parent::__construct(sprintf('There is no data for the given [%s] step.', $key));
+        parent::__construct(sprintf('There is no data for the given [%s] step.', $this->key));
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
     }
 }
